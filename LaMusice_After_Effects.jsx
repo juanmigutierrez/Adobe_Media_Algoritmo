5cd577e5114e37256eb04fc4d69110a97682eea1
juanmigutierrez <33558019+juanmigutierrez@users.noreply.github.com>
2019-01-15 21:52:51 -0500

    Update LaMusice_After_Effects.jsx

--- LaMusice_After_Effects.jsx
+++ LaMusice_After_Effects.jsx
@@ -1,20 +1,33 @@
-﻿//AFTER EFFECTS
+//AFTER EFFECTS
 
 // Abriendo After effects proyecto
 
 var proyecto = new File("C:\\Users\\Recup\\OneDrive\\Documentos\\Algoritmo_Adobe\\La_Musice_Youtube.aep")
+var Imagen_Texto = new File("C:\\Users\\Recup\\OneDrive\\Documentos\\Algoritmo_Adobe\\Direccion_Imagen.txt")
+var Song_Texto = new File("C:\\Users\\Recup\\OneDrive\\Documentos\\Algoritmo_Adobe\\Direccion_Cancion.txt")
 var render = new File("C:\\Users\\Recup\\OneDrive\\Documentos\\Algoritmo_Adobe\\Youtube.avi")
 
-var Imagen = File.openDialog ()
-var Song = File.openDialog()
+//var Imagen = File.openDialog () // Esto solo era para prueba, ahora se utiliza textos
+//var Song = File.openDialog()
 
-//$.writeln() Codigo para escribir consola
+Imagen_Texto.open("r")
+var Imagen_path = Imagen_Texto.readln()
+var Imagen = new File(Imagen_path)
+Imagen_Texto.close()
+
+Song_Texto.open("r")
+var path_Song = Song_Texto.readln()
+var Song = new File(path_Song)
+Song_Texto.close()
+
+//$.writeln() //Codigo para escribir consola
 
 //CANCION DURACION 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.open(proyecto)
+
 
 // app.project.item(1).file , sirve para sacar la direccion de los items
 // item(1) Imagen normal
@@ -23,8 +36,6 @@ app.open(proyecto)
 // item(4) Logo
 $.writeln("----")
 
-app.project.item(3).replace(Song)
-app.project.item(2).workAreaDuration = app.project.item(3).duration // Cambiarla despues por duration
 
 //replace sirve para remplazar items
 
@@ -32,8 +43,11 @@ app.project.item(1).replace(Imagen)
 var height = app.project.item(2).layer(2).height
 var width = app.project.item(2).layer(2).width
 
-//$.writeln(height)
-//$.writeln(width)
+app.project.item(3).replace(Song)
+app.project.item(2).workAreaDuration = app.project.item(3).duration // Cambiarla despues por duration
+
+//writeln(height)
+//writeln(width)
 
 var scale1 = 1920/width // Se divide sobre 1920 y 1080 debido a que es el tamano de la composition y se e utiliza max por que es un scale con misma proporcion
 var scale2 = 1080/height
@@ -42,13 +56,21 @@ var scalet = Math.max(scale1,scale2)*100 +1
 
 //$.writeln(scalet)
 
-app.project.item(2).layer(2).Scale.setValue([scalet,scalet,100])
-app.project.renderQueue.items.add(app.project.item(2))
-app.project.save()
+
+$.writeln(app.project.item(2).layer(3).Scale)
+var Comp = app.project.item(2)
+app.project.item(2).layer(3).Scale.setValue([scalet,scalet,100])
+app.project.renderQueue.items.add(Comp)
 app.project.renderQueue.item(1).outputModule(1).file = render
 app.project.renderQueue.item(1).outputModule(1).name
-app.project.renderQueue.render()
-app.project.close(CloseOptions.SAVE_CHANGES)
+
+app.project.renderQueue.queueInAME(true);
+$.sleep(1000)
+app.project.renderQueue.queueInAME(true);
+
+//app.project.renderQueue.render()
+
+app.project.close(CloseOptions.DO_NOT_SAVE_CHANGES)
 app.quit()
 
 // Primero se remplazara la imagen normal
@@ -56,5 +78,6 @@ app.quit()
 
 
 //$.writeln (app.project.items(1).file)
+
 
 
