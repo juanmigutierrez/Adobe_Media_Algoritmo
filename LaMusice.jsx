ec0910e3b67a53498c04825bbb1a903dc4a17cbd
juanmigutierrez <33558019+juanmigutierrez@users.noreply.github.com>
2019-01-15 21:51:47 -0500

    Update LaMusice.jsx

--- LaMusice.jsx
+++ LaMusice.jsx
@@ -1,3 +1,5 @@
+//AFTER EFFECTS
+
 var Photo = new File("C:\\Users\\Recup\\OneDrive\\Documentos\\Algoritmo_Adobe\\LaMusice_Photoshop.jsx");
 var After = new File("C:\\Users\\Recup\\OneDrive\\Documentos\\Algoritmo_Adobe\\LaMusice_After_Effects.jsx");
 var Imagen_Texto = new File("C:\\Users\\Recup\\OneDrive\\Documentos\\Algoritmo_Adobe\\Direccion_Imagen.txt");
@@ -12,7 +14,7 @@ Imagen_Texto.open("e")
 Imagen_Texto.write(Imagen.absoluteURI) // Recordar vaciar para que vuevla a funcionar
 Imagen_Texto.close()
 
-//photoshop.open(Photo)
+photoshop.open(Photo)
 
 Song_Texto.open("w")
 Song_Texto.close()
@@ -20,6 +22,5 @@ Song_Texto.open("e")
 Song_Texto.write(Song.absoluteURI)
 Song_Texto.close()
 
-After.execute()
 
 
